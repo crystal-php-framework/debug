06bd1585b044ba0bc1f702b688dc02d2c0a137a6
Nicholas E <isupozoy@gmail.com>
2018-08-23 02:01:46 -0500

    Create Debugger.php

---
+++ src/Debugger.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+/**
+ * Debugging could not be easy without the awesome crystal debugging component.
+ *
+ * @author  Nicholas English <isupozoy@gmail.com>.
+ * @license <https://github.com/crystal-php-framework/debug/blob/master/LICENSE> MIT License.
+ * @link    <https://github.com/crystal-php-framework/debug> Source Code.
+ */
+
+namespace Crystal\Debug\Exception;
+
+/**
+ * The main debugging controller.
+ */
+class Debugger implements DebuggerInterface
+{
+
+    /** @var ExceptionHandlerInterface The exception handler. */
+    private $exceptionHandler;
+    
+    /** @var ErrorHandlerInterface The error handler. */
+    private $errorHandler;
+
+    /**
+     * Construct a new debugging controller.
+     *
+     * @param ExceptionHandlerInterface $exceptionHandler The exception handler.
+     * @param ErrorHandlerInterface     $errorHandler     The error handler.
+     *
+     * @return void Returns nothing.
+     */
+    public function __construct(ExceptionHandlerInterface $exceptionHandler = null, ErrorHandlerInterface $errorHandler = null)
+    {
+        if (!is_null($exceptionHandler))
+        {
+            $this->exceptionHandler = $exceptionHandler;
+        }
+        if (!is_null($errorHandler))
+        {
+            $this->errorHandler = $errorHandler;
+        }
+    }
+}
