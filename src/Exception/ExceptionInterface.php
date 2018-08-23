d81d254b929ab21ea68048d4823267b6dc826767
Nicholas E <isupozoy@gmail.com>
2018-08-23 01:31:54 -0500

    Add the interface for all exceptions.

---
+++ src/Exception/ExceptionInterface.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * Debugging could not be easy without the awesome crystal debugging component.
+ *
+ * @author  Nicholas English <isupozoy@gmail.com>.
+ * @license <https://github.com/crystal-php-framework/debug/blob/master/LICENSE> MIT License.
+ * @link    <https://github.com/crystal-php-framework/debug> Source Code.
+ */
+
+namespace Crystal\Debug\Exception;
+
+use Throwable;
+
+/**
+ * This is the base interface for all exceptions.
+ */
+interface ExceptionInterface extends Throwable
+{
+}
