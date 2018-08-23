ebbddca7785369d5a5edf27be21820779449e0cd
Nicholas E <isupozoy@gmail.com>
2018-08-23 01:34:06 -0500

    Add the invalid operation exception.

---
+++ src/Exception/InvalidOperationException.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * Debugging could not be easy without the awesome crystal debugging component.
+ *
+ * @author  Nicholas English <isupozoy@gmail.com>.
+ * @license <https://github.com/crystal-php-framework/debug/blob/master/LICENSE> MIT License.
+ * @link    <https://github.com/crystal-php-framework/debug> Source Code.
+ */
+
+namespace Crystal\Debug\Exception;
+
+use RuntimeException;
+
+/**
+ * This exception is thrown when the requestion operation could not run.
+ */
+class InvalidOperationException extends RuntimeException implements ExceptionInterface
+{
+}
