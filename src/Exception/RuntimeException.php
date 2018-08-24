0de6b476ca8d46fcb169ea7804be9ed4e3ecd487
Nicholas E <isupozoy@gmail.com>
2018-08-23 20:36:48 -0500

    Add code.

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -1,1 +1,20 @@
+<?php
+declare(strict_types=1);
+/**
+ * Debugging could not be any easier without the awesome crystal debugging component.
+ *
+ * @author  Nicholas English <isupozoy@gmail.com>.
+ * @license <https://github.com/crystal-php-framework/debug/blob/master/LICENSE> MIT License.
+ * @link    <https://github.com/crystal-php-framework/debug> Source Code.
+ */
 
+namespace Crystal\Debug\Exception;
+
+use RuntimeException;
+
+/**
+ * This exception is thrown when the requestion operation could not run.
+ */
+class RuntimeException extends RuntimeException implements ExceptionInterface
+{
+}
