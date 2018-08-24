50f08f078ebd0de44fa3ed00e0d61a25c208e9f9
Nicholas E <isupozoy@gmail.com>
2018-08-23 21:26:31 -0500

    Update ExceptionHandlerInterface.php

--- src/ExceptionHandlerInterface.php
+++ src/ExceptionHandlerInterface.php
@@ -1,1 +1,18 @@
+<?php
+declare(strict_types=1);
+/**
+ * Debugging component from crystal framework.
+ *
+ * @author  Nicholas English <isupozoy@gmail.com>.
+ * @license <https://github.com/crystal-php-framework/debug/blob/master/LICENSE> MIT License.
+ * @link    <https://github.com/crystal-php-framework/debug> Source Code.
+ */
 
+namespace Crystal\Debug;
+
+/**
+ * The exception handler interface.
+ */
+interface ExceptionHandlerInterface
+{
+}
